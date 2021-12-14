00706071a627cff1c313356aa265b32e9f09bb39
Anuar Ustayev <anuar.ustayev@gmail.com>
2021-12-14 20:01:57 +0600

    [tiny cms][xl]: integration of tiny cms.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,9 @@ import { applyTheme } from '../themes/utils';
 import I18nProvider from 'next-translate/I18nProvider';
 import { useRouter } from 'next/router';
 import '../styles/globals.css';
+import dynamic from 'next/dynamic';
+import { TinaEditProvider } from 'tinacms/dist/edit-state';
+const TinaCMS = dynamic(() => import('tinacms'), { ssr: false });
 
 interface I8nObject {
   [property: string]: any;
@@ -46,7 +49,15 @@ const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
   return (
     <I18nProvider lang={router.locale} namespaces={pageProps._ns}>
       <ApolloProvider client={apolloClient}>
-        <Component {...pageProps} />
+        <TinaEditProvider
+          editMode={
+            <TinaCMS isLocalClient={true} {...pageProps}>
+              {(livePageProps) => <Component {...livePageProps} />}
+            </TinaCMS>
+          }
+        >
+          <Component {...pageProps} />
+        </TinaEditProvider>
       </ApolloProvider>
     </I18nProvider>
   );
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,35 +1,63 @@
-import { GetServerSideProps } from 'next';
+import parse from 'html-react-parser';
 import Head from 'next/head';
-import { initializeApollo } from '../../lib/apolloClient';
 import Nav from '../../components/home/Nav';
-import List from '../../components/static/List';
-import { GET_POSTS_QUERY } from '../../graphql/queries';
+import { getStaticPropsForTina } from 'tinacms';
 
-const PostList: React.FC = () => (
-  <>
-    <Head>
-      <title>Portal | Blog</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Nav />
-    <main className="p-6">
-      <List />
-    </main>
-  </>
-);
+export default function Home(props) {
+  const postsList = props.data.getPostList.edges;
+  return (
+    <>
+      <Head>
+        <title>Portal | Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <main className="p-6">
+        <h1 className="text-3xl font-semibold text-primary my-6 inline-block">
+          {postsList.length} articles found
+        </h1>
+        {postsList.map((post) => (
+          <div key={post.node.id}>
+            <a
+              href={`/blog/${post.node.sys.filename}`}
+              className="text-2xl font-semibold text-primary my-6 inline-block"
+            >
+              {parse(post.node.sys.filename)}
+            </a>
+            <p>
+              {parse(
+                post.node.values
+                  ? post.node.values.title
+                  : post.node.sys.filename
+              )}
+            </p>
+          </div>
+        ))}
+      </main>
+    </>
+  );
+}
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const apolloClient = initializeApollo();
-
-  await apolloClient.query({
-    query: GET_POSTS_QUERY,
+export const getStaticProps = async () => {
+  const tinaProps = await getStaticPropsForTina({
+    query: `{
+        getPostList{
+          edges {
+            node {
+              id
+              sys {
+                filename
+              }
+            }
+          }
+        }
+      }`,
+    variables: {},
   });
 
   return {
     props: {
-      initialApolloState: apolloClient.cache.extract(),
+      ...tinaProps,
     },
   };
 };
-
-export default PostList;
