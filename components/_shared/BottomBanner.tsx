0d2c48c2f55148bdb11fe74d28e634ddffb22541
virgoaugustine <virgo.anankum@datopian.com>
2022-02-05 20:17:40 +0000

    updates to BottomBanner

--- components/_shared/BottomBanner.tsx
+++ components/_shared/BottomBanner.tsx
@@ -1,12 +1,12 @@
 export default function BottomBanner() {
   return (
     <div className="relative pb-32 pt-24">
-      <div className="bg-red-50 p-20 rounded-2xl w-1/2">
-        <span className="text-4xl font-semibold">
+      <div className="bg-red-50 px-8 py-20 rounded-2xl w-1/2">
+        <span className="text-4xl font-semibold mx-4">
           Still not found <br /> what you&apos;re looking for ...?
         </span>
       </div>
-      <div className="flex flex-col bg-black w-2/3 rounded-l-3xl px-24 py-20 absolute bottom-36 sm:bottom-24 -right-4">
+      <div className="flex flex-col bg-black w-3/5 rounded-l-3xl px-24 py-20 absolute bottom-36 sm:bottom-24 -right-4">
         <span className="bg-green-300 rounded-full w-24 h-6 px-2">
           Next Stop
         </span>
