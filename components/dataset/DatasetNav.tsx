9e0fbf30216a2c6423930882acda78c6adf3e940
virgoaugustine <virgo.anankum@datopian.com>
2022-02-07 16:36:54 +0000

    implement resources component

--- components/dataset/DatasetNav.tsx
+++ components/dataset/DatasetNav.tsx
@@ -5,29 +5,36 @@ export default function DatasetNav({ setNavBody }) {
   };
   return (
     <div className="flex">
-      <div
-        className="border-b-8 border-gray-100 px-8 py-4 btn"
-        onClick={() => handleNav('overview')}
-      >
-        <button className="font-medium tracking-wide text-gray-400">
+      <div className="border-b-8 border-gray-100 px-8 py-4 hover:border-darkbrown">
+        <button
+          className="font-medium tracking-wide text-gray-400 appearance-none focus:outline-none"
+          onClick={() => handleNav('overview')}
+        >
           Overview
         </button>
       </div>
-      <div className="border-b-8 border-gray-100 px-8 py-4" onClick={() => handleNav('resources')}>
-        <span className="font-medium tracking-wide text-gray-400">
+      <div className="border-b-8 border-gray-100 px-8 py-4 hover:border-darkbrown">
+        <button
+          className="font-medium tracking-wide text-gray-400 appearance-none focus:outline-none"
+          onClick={() => handleNav('resources')}
+        >
           Resources
-        </span>
+        </button>
       </div>
-      <div className="border-b-8 border-gray-100 px-8 py-4">
-        <span className="font-medium tracking-wide text-gray-400" >
+      <div className="border-b-8 border-gray-100 px-8 py-4 hover:border-darkbrown">
+        <span className="font-medium tracking-wide text-gray-400 appearance-none focus:outline-none">
           Activity Stream
         </span>
       </div>
-      <div className="border-b-8 border-gray-100 px-8 py-4" >
-        <span className="font-medium tracking-wide text-gray-400">Stats</span>
+      <div className="border-b-8 border-gray-100 px-8 py-4 hover:border-darkbrown">
+        <span className="font-medium tracking-wide text-gray-400 appearance-none focus:outline-none">
+          Stats
+        </span>
       </div>
-      <div className="border-b-8 border-gray-100 px-8 py-4">
-        <span className="font-medium tracking-wide text-gray-400" >More</span>
+      <div className="border-b-8 border-gray-100 px-8 py-4 hover:border-darkbrown">
+        <span className="font-medium tracking-wide text-gray-400 appearance-none focus:outline-none">
+          More
+        </span>
       </div>
     </div>
   );
