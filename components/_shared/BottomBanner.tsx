9d79e5d2778ee35d43e61bd74c876f62549107de
virgoaugustine <virgo.anankum@datopian.com>
2022-02-23 14:53:45 +0000

    adjust bottom banner

--- components/_shared/BottomBanner.tsx
+++ components/_shared/BottomBanner.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 export default function BottomBanner() {
   return (
     <div className="relative pb-32 mt-32">
@@ -6,17 +8,25 @@ export default function BottomBanner() {
           Still not found <br /> what you&apos;re looking for ...?
         </span>
       </div>
-      <div className="flex flex-col bg-banner w-3/5 rounded-l-3xl px-24 py-20 absolute bottom-36 sm:bottom-24 -right-4">
-        <span className="bg-green-500 rounded-full w-24 h-6 px-2">
-          Next Stop
-        </span>
-        <span className="text-3xl sm:text-6xl font-bold text-white mt-4">
-          Themes -&gt;
-        </span>
-        <span className="text-sm font-bold text-gray-100 sm:w-2/3 mt-4">
-          Explore the world of open data on bayanat. We provide the best
-          exprience to data anlysists, students and developers
-        </span>
+      <div className="w-3/5 px-20 py-14 absolute bottom-36 sm:bottom-28 -right-4 mr-4">
+        <Image
+          layout="fill"
+          className="object-center object-cover pointer-events-none rounded-l-3xl"
+          src="/images/banner.jpg"
+          alt="bottom banner"
+        />
+        <div className="flex flex-col relative z-10">
+          <span className="bg-green-500 rounded-full w-24 h-6 px-2">
+            Next Stop
+          </span>
+          <span className="text-3xl sm:text-6xl font-bold text-white mt-4">
+            Themes -&gt;
+          </span>
+          <span className="text-sm font-bold text-gray-100 sm:w-2/3 mt-4">
+            Explore the world of open data on bayanat. We provide the best
+            exprience to data anlysists, students and developers
+          </span>
+        </div>
       </div>
     </div>
   );
