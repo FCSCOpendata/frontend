392b2fdf1ca6dc5128ab62d5cdfc82de8e01162a
virgoaugustine <virgo.anankum@datopian.com>
2022-02-24 15:46:43 +0000

    make search suggestions dynamic

--- components/search/SearchSuggestions.tsx
+++ components/search/SearchSuggestions.tsx
@@ -1,8 +1,19 @@
 import { useQuery } from '@apollo/react-hooks';
 import { GET_POPULAR_DATASETS_QUERY } from '../../graphql/queries';
+import { ErrorMessage } from '../_shared';
 
 const SearchSuggestions: React.FC = () => {
-  // const { data, loading, error } = useQuery(GET_POPULAR_DATASETS_QUERY);
+  const { loading, error, data } = useQuery(GET_POPULAR_DATASETS_QUERY, {
+    notifyOnNetworkStatusChange: true,
+  });
+
+  if (loading) return <div>Loading Search Suggestions</div>;
+  if (error)
+    return <ErrorMessage message="Error loading search suggestions" />;
+
+  const result = data.popular.result.results;
+
+  console.log(result);
 
   return (
     <div className="mt-16 mb-32">
@@ -10,15 +21,14 @@ const SearchSuggestions: React.FC = () => {
         Search Suggestions
       </h3>
       <div className=" text-xs text-blue-800 font-bold space-x-2">
-        <div className="inline-block bg-blue-100 items-center rounded-2xl capitalize px-3 py-1 mb-2">
-          Data Policies of UAE
-        </div>
-        <div className="inline-block bg-blue-100 items-center rounded-2xl capitalize px-3 py-1 mb-2 ">
-          Covid-19 Testing in Dubai
-        </div>
-        <div className="inline-block bg-blue-100 items-center rounded-2xl capitalize px-3 py-1 mb-2 ">
-          Child Count and Educational Environment
-        </div>
+        {result.map((dataset, index) => (
+          <div
+            key={index}
+            className="inline-block bg-blue-100 items-center rounded-2xl capitalize px-3 py-1 mb-2"
+          >
+            {dataset.title}
+          </div>
+        ))}
       </div>
     </div>
   );
