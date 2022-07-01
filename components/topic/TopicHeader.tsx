2e769bdc74348c71d0b636debdc97bd79d236295
Deme <joaommdemenech@gmail.com>
2022-07-01 20:29:25 -0300

    Adds background header background color

--- components/topic/TopicHeader.tsx
+++ components/topic/TopicHeader.tsx
@@ -1,10 +1,35 @@
 const TopicHeader: React.FC<any> = ({ topic }) => {
   return (
     <>
-      <div className="grid grid-cols-2">
-        <div className="w-full">
+      <div className="grid grid-cols-2 relative">
+        <div
+          className="
+          absolute 
+          h-full 
+          w-full 
+          pt-[50px] 
+          pb-[50px] 
+          ml-[5rem] 
+          z-0
+          "
+        >
+          <div className="bg-[#F7FAFC] h-full w-full"></div>
+        </div>
+        <div className="w-full z-10">
           {/* WIP */}
-          <div className="bg-[url('https://coolmagazine.com.br/wp-content/uploads/2022/05/dubai.png')] h-[500px] w-100 bg-center bg-no-repeat bg-cover"></div>
+          <div
+            className="
+              bg-[url('https://coolmagazine.com.br/wp-content/uploads/2022/05/dubai.png')] 
+              h-[650px] 
+              ml-[-5rem]
+              mr-[5rem]
+              w-100 
+              bg-center 
+              bg-no-repeat 
+              bg-cover 
+              rounded-r-[50px]
+            "
+          ></div>
         </div>
         <div className="w-full">
           <div>{topic.title}</div>
