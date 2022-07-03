d0e5bc0ce37e8efb685ec28cd33984ccb149013e
Anuar Ustayev <anuar.ustayev@gmail.com>
2022-07-04 01:21:35 +0600

    [home/open-data-101][s]: implemented open-data-101 section.

--- components/home/main/OpenData101.tsx
+++ components/home/main/OpenData101.tsx
@@ -1,7 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 export default function OpenData101() {
+  function scrollToTop() {
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+  }
+
   return (
-    <div className="h-fit">
+    <div className="relative h-fit">
       <img src="/images/open-data-101.svg" alt="Open Data 101" />
+      <div className="absolute w-1/2 inset-x-1/4 inset-y-1/2 text-center">
+        <p className="text-[#54CA59] font-semibold">- NEXT STOP -</p>
+        <a href="/open-data-101">
+          <h2 className="font-medium text-4xl mb-2">Open Data 101</h2>
+        </a>
+        <p className="text-center px-28 text-sm mb-4">
+          Learn more about what open data is, how to use it and other key
+          things you need to know.
+        </p>
+        <a
+          href="#"
+          className="text-[#54CA59] font-medium"
+          onClick={scrollToTop}
+        >
+          Go back to top
+        </a>
+      </div>
     </div>
   );
 }
