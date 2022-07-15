795866dcebadb87d5916a77c4208c655bb258ffb
Deme <joaommdemenech@gmail.com>
2022-07-14 22:13:29 -0300

    [Search/Scroll Indicator][m]: adds scroll indicator to search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -6,6 +6,7 @@ import List from '../components/search/List';
 import OpenData101 from '../components/home/main/OpenData101';
 import { useState } from 'react';
 import DeveloperExperience from '../components/_shared/developer_experience/DeveloperExperience';
+import ScrollIndicator from '../components/_shared/ScrollIndicator';
 
 type Props = {
   variables: any;
@@ -24,17 +25,29 @@ const Search: React.FC<Props> = ({ variables }) => {
       <Head>
         <title>Search Datasets | Open Data UAE</title>
       </Head>
+      <ScrollIndicator
+        firstImage={{
+          url: '/images/scroll_indicator_icon_1.svg',
+          alt: 'First stop',
+        }}
+        lastImage={{
+          url: '/images/scroll_indicator_icon_1.svg',
+          alt: 'First stop',
+        }}
+        stops={[{ id: 'search' }, { id: 'dev-exp' }]}
+      />
+
       <Form
         variables={qvariables}
         setQvariables={setQvariables}
         setSideFilter={setSideFilter}
       />
-      <div className="mb-12">
+      <div className="mb-12" id="search">
         <div className="px-4">
           <List variables={qvariables} setQvariables={setQvariables} />
         </div>
       </div>
-      <div className="px-16">
+      <div className="px-16" id="dev-exp">
         <DeveloperExperience />
       </div>
       <OpenData101 />
