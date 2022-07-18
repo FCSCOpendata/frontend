624a7a268a4f725623a1c862a600d229c4044061
Deme <joaommdemenech@gmail.com>
2022-07-18 18:56:32 -0300

    [News Page/Suggested Reads][lg]: adds suggested reads to news -- refs #54

---
+++ components/static/SuggestedReads.tsx
@@ -0,0 +1,55 @@
+import { useQuery } from '@apollo/react-hooks';
+import { GET_NEXT_POSTS_QUERY } from '../../graphql/queries';
+import { ErrorMessage, Spinner } from '../_shared';
+
+const SuggestedReads: React.FC<any> = ({ from }) => {
+  if (!from || !Object.keys(from).length) return <></>;
+
+  const slug = from.slug;
+  const after = new Date(from.published).toISOString();
+
+  const { loading, error, data } = useQuery(GET_NEXT_POSTS_QUERY, {
+    variables: { limit: 4, after, slug },
+  });
+
+  if (!loading && !error && !data?.posts.posts.length) return <></>;
+
+  return (
+    <>
+      <div className="my-20 py-20 mx-5 md:mx-28">
+        <div className="block mx-auto">
+          <h1 className="font-semibold text-2xl px-1">Next reads</h1>
+          {error && <ErrorMessage message="Error loading suggested reads" />}
+          {loading && (
+            <div className="w-full flex justify-center">
+              <Spinner className="mt-10" size="10" />
+            </div>
+          )}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {data?.posts.posts.map((suggestion, index) => (
+              <a
+                key={index}
+                href={`/news/${suggestion.slug}`}
+                className="m-1 relative"
+              >
+                <div className="aspect-w-16 aspect-h-9 m-1 relative">
+                  <img
+                    src={suggestion.image}
+                    width="100%"
+                    alt={suggestion.title}
+                    className="z-10 object-cover mx-auto"
+                  />
+                </div>
+                <h1 className="absolute bottom-2 left-1 z-20 font-semibold text-[#fff] px-4 lg:pb-2">
+                  {suggestion.title}
+                </h1>
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SuggestedReads;
--- pages/news/[slug]/index.tsx
+++ pages/news/[slug]/index.tsx
@@ -1,16 +1,23 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
 import Post from '../../../components/static/Post';
+import SuggestedReads from '../../../components/static/SuggestedReads';
 
-const PageItem: React.FC<{ slug: string }> = ({ slug }) => (
-  <>
-    <Head>
-      <title>Portal | {slug}</title>
-      <link rel="icon" href="/favicon.svg" />
-    </Head>
-    <Post slug={slug} />
-  </>
-);
+const PageItem: React.FC<{ slug: string }> = ({ slug }) => {
+  const [post, setPost] = useState({});
+
+  return (
+    <>
+      <Head>
+        <title>Portal | {slug}</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <Post slug={slug} setPost={setPost} />
+      <SuggestedReads from={post} />
+    </>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const slug = context.query.slug;
