875358ac3af2c729e8fd8daeceede08e406c5c94
Deme <joaommdemenech@gmail.com>
2022-07-19 09:55:51 -0300

    [Orgs Page][s]: makes open-data-101 full-width -- refs #85

--- pages/[org]/index.tsx
+++ pages/[org]/index.tsx
@@ -104,11 +104,11 @@ const Organization: React.FC<any> = ({ variables }) => {
           <div id="developer-experience">
             <DeveloperExperience />
           </div>
-          <div id="open-data-101">
-            <OpenData101 />
-          </div>
         </div>
       </main>
+      <div id="open-data-101">
+        <OpenData101 />
+      </div>
     </>
   );
 };
