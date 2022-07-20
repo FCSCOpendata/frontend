94d5e9ea40320c3f31823d49af1a84e3dd4264db
steveoni <steohenoni2@gmail.com>
2022-07-20 17:19:27 +0100

    add .fq condition to update api result

--- pages/search.tsx
+++ pages/search.tsx
@@ -95,10 +95,10 @@ const Search: React.FC<Props> = ({ variables }) => {
       <div className="mx-10 md:mx-28" id="dev-exp">
         <DeveloperExperience
           api={
-            qvariables.q
-              ? `${window.location.origin}/api/search?q=${encodeURI(
-                  qvariables.q
-                )}&fq=${qvariables.fq}`
+            qvariables.q || qvariables.fq
+              ? `${window.location.origin}/api/search?q=${
+                  qvariables.q ? encodeURI(qvariables.q) : ''
+                }&fq=${qvariables.fq}`
               : `${window.location.origin}/api/search?q=dataset_name`
           }
         />
