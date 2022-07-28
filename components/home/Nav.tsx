3e98c9788b6f4836d83d976bf55e42a1f8f3f8ed
Deme <joaommdemenech@gmail.com>
2022-07-28 19:42:07 -0300

    [Dynamic Nav/Footer Items][s]: Leaving some comments for better documentation

--- components/home/Nav.tsx
+++ components/home/Nav.tsx
@@ -1,9 +1,16 @@
 import { useEffect, useState } from 'react';
 import Template from './NavTemplate';
 
+//  This variable is for listing the hrefs that
+//  are not related to CMS pages.  Furthermore,
+//  This is needed to check if there should  be
+//  a /p/ at the final URL or not.
 export const dynamicPages = ['search', 'organization', 'topic', 'news'];
 
 const NavBar: React.FC<any> = ({ settings }) => {
+  //  This initial state behaves as a default,
+  //  just in case the servers are unavailable
+  //  or something like that.
   const [navMenu, setNavMenu] = useState([
     { title: 'Datasets', path: '/search' },
     { title: 'Organizations', path: '/organization', matchExp: '/@' },
@@ -19,12 +26,17 @@ const NavBar: React.FC<any> = ({ settings }) => {
         // Gets the last segment of the URL
         let path;
 
+        //  If the path has a `//` it should be
+        //  an absolute path
         if (nav.url.includes('//')) {
+          //  Gets only the last segment, after
+          //  the last '/'
           const tmp = /[^/]*\w*$/.exec(nav.url);
           path = tmp.length > 0 ? tmp[0] : '';
         } else path = nav.url;
 
         path = path.replaceAll('/', '');
+        //  Adds the '/p/' if necessary
         path = `${!dynamicPages.includes(path) ? '/p' : ''}/${path}`;
 
         return {
@@ -32,6 +44,7 @@ const NavBar: React.FC<any> = ({ settings }) => {
           path: path.length > 0 ? path : null,
         };
       })
+      //  Filters empty paths
       .filter((el) => el.path != '');
 
     if (cmsNavigation) {
