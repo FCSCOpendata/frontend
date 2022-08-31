f46e13c261006ba89b29bd1c5a71d4a182925d96
steveoni <steohenoni2@gmail.com>
2022-08-31 15:13:09 +0100

    update translation

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,19 +1,22 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import List from '../../components/static/List';
+import useTranslation from 'next-translate/useTranslation';
 
-const News: React.FC = () => (
-  <>
-    <Head>
-      <title>News | Open Data UAE</title>
-      <link rel="icon" href="/favicon.svg" />
-    </Head>
-    <div className="relative bg-[#F7FAFC] font-[Avenir] flex flex-col items-center justify-center w-full py-6 overflow-hidden">
-      <div className="absolute bg-waves bg-cover bg-no-repeat bg-center left-0 right-0 top-[-227%] bottom-[-109%] z-0" />
-      <h1 className="text-3xl font-extrabold">News</h1>
-    </div>
-    <List />
-  </>
-);
+const News: React.FC = () => {
+  const { t } = useTranslation('common');
+  return (
+    <>
+      <Head>
+        <title>News | Open Data UAE</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <div className="relative bg-[#F7FAFC] font-[Avenir] flex flex-col items-center justify-center w-full py-6 overflow-hidden">
+        <div className="absolute bg-waves bg-cover bg-no-repeat bg-center left-0 right-0 top-[-227%] bottom-[-109%] z-0" />
+        <h1 className="text-3xl font-extrabold">{t('news')}</h1>
+      </div>
+      <List />
+    </>
+  );
+};
 
 export default News;
