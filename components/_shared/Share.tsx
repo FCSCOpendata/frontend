e08222c3515d56ad1d559880fc768338c346cd77
steveoni <steohenoni2@gmail.com>
2022-09-05 11:42:57 +0100

    [share][s]: update icon size

--- components/_shared/Share.tsx
+++ components/_shared/Share.tsx
@@ -10,13 +10,15 @@ const Share: React.FC<{ title: string }> = ({ title }) => {
       <div className="font-avenir text-bold p-2">{t('share-dataset')}</div>
       <div className="font-avenir text-sm p-4">
         {title} -{' '}
-        <span className={` ${AR('ml-2', 'mr-2')}}`}>
+        <span className={`text-[#1F356C] ${AR('ml-2', 'mr-2')}}`}>
+          {typeof window !== 'undefined' ? window.location.href : ''}
+          &nbsp;
           <a
             href={`mailto:?subject=${subject}&body=${title} - ${
               typeof window !== 'undefined' ? window.location.href : ''
             }`}
           >
-            <ShareIcon className="text-green-400 w-4 inline" />
+            <ShareIcon className="text-green-400 w-7 h-7  inline" />
           </a>
         </span>
       </div>
