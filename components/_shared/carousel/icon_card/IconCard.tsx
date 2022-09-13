a7586186211e4b74e5e1d6ee117e6eee7b153d10
deme <joaommdemenech@gmail.com>
2022-09-13 15:47:21 -0300

    [Topics/Orgs Carousel][s]: images now use Next.js Image component -- refs #99

--- components/_shared/carousel/icon_card/IconCard.tsx
+++ components/_shared/carousel/icon_card/IconCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 interface IconCardProps {
   title: string;
   icon: {
@@ -22,9 +24,15 @@ const IconCard: React.FC<IconCardProps> = ({
 
   return (
     <>
-      <div className={`${isActive ? 'text-[#22B373]' : ''}`}>
-        <div className="h-[60px] flex justify-center">
-          <img src={icon.url || '/images/no-image.svg'} alt={icon.alt} />
+      <div className={`${isActive ? 'text-[#22B373]' : ''} flex flex-col items-center`}>
+        <div className="h-[60px] w-[60px]">
+          <Image
+            width={60}
+            height={60}
+            src={icon.url || '/images/no-image.svg'}
+            alt={icon.alt}
+            className="object-contain"
+          />
         </div>
         <h3
           className="font-avenir font-medium text-md text-center mt-4"
