efd5d4fb31bd12631196602bcf6dd7ca12dc7323
deme <joaommdemenech@gmail.com>
2022-09-21 10:32:28 -0300

    Linting

--- components/resource/DashboardBuilder.tsx
+++ components/resource/DashboardBuilder.tsx
@@ -18,7 +18,9 @@ const DashboardBuilder: React.FC<{ resource: any }> = ({ resource }) => {
           text: 'Copy and paste OData URL for desired resource, see the sample',
           url: `${
             typeof window !== 'undefined'
-              ? window.location.origin + '/api/datastore/odata3.0/' + resource.id
+              ? window.location.origin +
+                '/api/datastore/odata3.0/' +
+                resource.id
               : ''
           }`,
         },
