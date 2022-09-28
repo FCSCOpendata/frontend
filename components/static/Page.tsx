2d8db5096059b8ebccb2202274f12e0c1a2d76ac
deme <joaommdemenech@gmail.com>
2022-09-28 10:43:46 -0300

    [Page][xs]: removes Created At and Read Time fields

--- components/static/Page.tsx
+++ components/static/Page.tsx
@@ -35,26 +35,6 @@ const Page: React.FC<{ slug: string }> = ({ slug }) => {
       <div className="relative bg-[#F7FAFC] font-avenir flex flex-col items-center justify-center w-full py-6 overflow-hidden">
         <div className="absolute bg-waves bg-cover bg-no-repeat bg-center left-0 right-0 top-[-227%] bottom-[-109%] z-0" />
         <h1 className="text-3xl font-extrabold z-10">{title}</h1>
-        <div className="inline-flex items-center justify-center sm:justify-start py-1 xl:py-2 space-x-2 text-[#7C7C7C]">
-          <div className={`${AR('ml-4', 'mr-4')}`}>
-            <img
-              src="/images/time.svg"
-              alt="reading time"
-              className={`inline w-4 pb-1 grayscale ${AR('ml-1', 'mr-1')}`}
-            />
-            <span className="text-xs text-center sm:text-left">
-              {readingTime} min read
-            </span>
-          </div>
-          <div>
-            <CalendarIcon
-              className={`inline pb-1 w-4 ${AR('ml-1', 'mr-1')}`}
-            />
-            <span className="text-xs text-center sm:text-left">
-              {timeago.format(published)}
-            </span>
-          </div>
-        </div>
       </div>
       <div className="my-10 grid place-content-center font-avenir">
         <article className="prose prose-stone md:prose-lg lg:prose-xl">
