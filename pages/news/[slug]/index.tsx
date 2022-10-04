d828cc979c52b27cb3a1f893f3de798027020200
deme <joaommdemenech@gmail.com>
2022-10-04 08:37:07 -0300

    [Post][xs]: fixes post language switching

--- pages/news/[slug]/index.tsx
+++ pages/news/[slug]/index.tsx
@@ -2,6 +2,8 @@ import { useQuery } from '@apollo/react-hooks';
 import { GetServerSideProps } from 'next';
 import useTranslation from 'next-translate/useTranslation';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Post from '../../../components/static/Post';
 import SuggestedReads from '../../../components/static/SuggestedReads';
 import { GET_POST_QUERY } from '../../../graphql/queries';
@@ -9,10 +11,18 @@ import { initializeApollo } from '../../../lib/apolloClient';
 
 const PageItem: React.FC<{ slug: string }> = ({ slug }) => {
   const { t } = useTranslation('common');
+  const router = useRouter();
 
   const { data } = useQuery(GET_POST_QUERY, {
     variables: { slug },
   });
+
+  //  Ensures that the URL is right after
+  //  switching language
+  useEffect(() => {
+    router.query.slug = slug;
+    router.push(router, null, { shallow: true });
+  }, [router.locale]);
 
   const post = data?.post?.posts[0];
 
@@ -31,14 +41,29 @@ const PageItem: React.FC<{ slug: string }> = ({ slug }) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const slug = context.query.slug;
+  let slug = context.query.slug as string;
+
+  //  It's needed to ensure that when the language
+  //  is set to Arabic the Arabic posts are pulled
+  if (context.locale.toLowerCase() == 'ar') {
+    //  Checks if current slug starts with 'ar-'
+    if (!slug.startsWith('ar-')) {
+      slug = `ar-${slug}`;
+    }
+  } else {
+    if (slug.startsWith('ar-')) {
+      slug = slug.replace('ar-', '');
+    }
+  }
 
   const apolloClient = initializeApollo();
 
-  await apolloClient.query({
+  const queryParams = {
     query: GET_POST_QUERY,
     variables: { slug },
-  });
+  };
+
+  await apolloClient.query(queryParams);
 
   return {
     props: {
